31e0a2b887c59bcb50fefe703f42056f54087909
VTracyHuang <861522731@qq.com>
2019-11-25 18:30:32 +0800

    add entity

---
+++ GitTest/src/com/woniu/Git.java
@@ -0,0 +1,13 @@
+package com.woniu;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Git {
+	private String gname;
+	private int addr;
+}
